c74225ddcda13ed48b6404e2d0b86ae485023a00
David Larrimore <david.larrimore@gsa.gov>
2015-06-08 23:46:28 -0400

    Working Django

--- manage.py
+++ manage.py
@@ -7,4 +7,11 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    ## added to support mysql on OSX
+    try:
+        import pymysql
+        pymysql.install_as_MySQLdb()
+    except ImportError:
+        pass
+
     execute_from_command_line(sys.argv)
--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST' : 'localhost' ,
+        'USER' : 'mixfin',
+        'PASSWORD' : '5CK&pKnJ8TJR',
+        'NAME': 'mixfin',
     }
 }
 
